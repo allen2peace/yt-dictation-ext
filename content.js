15f2a1c2e28b7b0651860da742e768a1c2312f83
caobainan <allencao@lookmeimg.com>
2024-09-25 16:05:43 +0800

    add subtitle

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+import $ from "jquery";
+
 let loopStart = null;
 let loopEnd = null;
 let videoElement = null;
@@ -117,7 +119,7 @@ function addButtonToYouTubePlayer() {
   console.log('按钮已添加到控制栏');
 }
 
-function createOverlayPlayer() {
+async function createOverlayPlayer() {
   console.log('按钮被点击了!');
   
   const videoElement = document.querySelector('video');
@@ -188,12 +190,176 @@ function createOverlayPlayer() {
   });
 
   console.log('遮罩层和视频已添加');
+
+  //请求视频字幕信息
+  const videoId = getYouTubeVideoId();
+  console.log('视频ID:', videoId);
+  const langOptionsWithLink = await getLangOptionsWithLink(videoId);
+  console.log('字幕信息:', langOptionsWithLink);
+  if (!langOptionsWithLink) {
+    console.log('没有字幕信息');
+      // noTranscriptionAlert();
+      return;
+  }
+
+  const transcriptHTML = await getTranscriptHTML(langOptionsWithLink[0].link, videoId);
+  console.log('字幕HTML:', transcriptHTML);
+
 }
 
 // 辅助函数：获取YouTube视频ID
 function getYouTubeVideoId() {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get('v');
+}
+
+async function getLangOptionsWithLink(videoId) {
+  // Get a transcript URL
+  const videoPageResponse = await fetch("https://www.youtube.com/watch?v=" + videoId);
+  const videoPageHtml = await videoPageResponse.text();
+  const splittedHtml = videoPageHtml.split('"captions":')
+
+  if (splittedHtml.length < 2) { return; } // No Caption Available
+
+  const captions_json = JSON.parse(splittedHtml[1].split(',"videoDetails')[0].replace('\n', ''));
+  const captionTracks = captions_json.playerCaptionsTracklistRenderer.captionTracks;
+  const languageOptions = Array.from(captionTracks).map(i => { return i.name.simpleText; })
+  
+  const first = "English"; // Sort by English first
+  languageOptions.sort(function(x,y){ return x.includes(first) ? -1 : y.includes(first) ? 1 : 0; });
+  languageOptions.sort(function(x,y){ return x == first ? -1 : y == first ? 1 : 0; });
+
+  return Array.from(languageOptions).map((langName, index) => {
+    const link = captionTracks.find(i => i.name.simpleText === langName).baseUrl;
+    return {
+      language: langName,
+      link: link
+    }
+  })
+}
+
+async function getTranscriptHTML(link, videoId) {
+
+  const rawTranscript = await getRawTranscript(link);
+
+  const scriptObjArr = [], timeUpperLimit = 60, charInitLimit = 300, charUpperLimit = 500;
+  let loop = 0, chars = [], charCount = 0, timeSum = 0, tempObj = {}, remaining = {};
+
+  // Sum-up to either total 60 seconds or 300 chars.
+  Array.from(rawTranscript).forEach((obj, i, arr) => {
+
+      // Check Remaining Text from Prev Loop
+      if (remaining.start && remaining.text) {
+          tempObj.start = remaining.start;
+          chars.push(remaining.text);
+          remaining = {}; // Once used, reset to {}
+      }
+
+      // Initial Loop: Set Start Time
+      if (loop == 0) {
+          tempObj.start = (remaining.start) ? remaining.start : obj.start;
+      }
+
+      loop++;
+
+      const startSeconds = Math.round(tempObj.start);
+      const seconds = Math.round(obj.start);
+      timeSum = (seconds - startSeconds);
+      charCount += obj.text.length;
+      chars.push(obj.text);
+
+      if (i == arr.length - 1) {
+          tempObj.text = chars.join(" ").replace(/\n/g, " ");
+          scriptObjArr.push(tempObj);
+          resetNums();
+          return;
+      }
+
+      if (timeSum > timeUpperLimit) {
+          tempObj.text = chars.join(" ").replace(/\n/g, " ");
+          scriptObjArr.push(tempObj);
+          resetNums();
+          return;
+      }
+
+      if (charCount > charInitLimit) {
+
+          if (charCount < charUpperLimit) {
+              if (obj.text.includes(".")) {
+
+                  const splitStr = obj.text.split(".");
+
+                  // Case: the last letter is . => Process regulary
+                  if (splitStr[splitStr.length-1].replace(/\s+/g, "") == "") {
+                      tempObj.text = chars.join(" ").replace(/\n/g, " ");
+                      scriptObjArr.push(tempObj);
+                      resetNums();
+                      return;
+                  }
+
+                  // Case: . is in the middle
+                  // 1. Get the (length - 2) str, then get indexOf + str.length + 1, then substring(0,x)
+                  // 2. Create remaining { text: str.substring(x), start: obj.start } => use the next loop
+                  const lastText = splitStr[splitStr.length-2];
+                  const substrIndex = obj.text.indexOf(lastText) + lastText.length + 1;
+                  const textToUse = obj.text.substring(0,substrIndex);
+                  remaining.text = obj.text.substring(substrIndex);
+                  remaining.start = obj.start;
+
+                  // Replcae arr element
+                  chars.splice(chars.length-1,1,textToUse)
+                  tempObj.text = chars.join(" ").replace(/\n/g, " ");
+                  scriptObjArr.push(tempObj);
+                  resetNums();
+                  return;
+
+              } else {
+                  // Move onto next loop to find .
+                  return;
+              }
+          }
+
+          tempObj.text = chars.join(" ").replace(/\n/g, " ");
+          scriptObjArr.push(tempObj);
+          resetNums();
+          return;
+
+      }
+
+  })
+
+  return Array.from(scriptObjArr).map(obj => {
+      const t = Math.round(obj.start);
+      const hhmmss = convertIntToHms(t);
+      return  `<div class="yt_ai_summary_transcript_text_segment">
+                  <div><a class="yt_ai_summary_transcript_text_timestamp" style="padding-top: 16px !important;" href="/watch?v=${videoId}&t=${t}s" target="_blank" data-timestamp-href="/watch?v=${videoId}&t=${t}s" data-start-time="${t}">${hhmmss}</a></div>
+                  <div class="yt_ai_summary_transcript_text" data-start-time="${t}">${obj.text}</div>
+              </div>`
+  }).join("");
+
+  function resetNums() {
+      loop = 0, chars = [], charCount = 0, timeSum = 0, tempObj = {};
+  }
+}
+
+async function getRawTranscript(link) {
+
+  // Get Transcript
+  const transcriptPageResponse = await fetch(link); // default 0
+  const transcriptPageXml = await transcriptPageResponse.text();
+
+  // Parse Transcript
+  const jQueryParse = $.parseHTML(transcriptPageXml);
+  const textNodes = jQueryParse[1].childNodes;
+
+  return Array.from(textNodes).map(i => {
+    return {
+      start: i.getAttribute("start"),
+      duration: i.getAttribute("dur"),
+      text: i.textContent
+    };
+  });
+
 }
 
 // 监听页面变化,确保在视频加载后添加按钮
